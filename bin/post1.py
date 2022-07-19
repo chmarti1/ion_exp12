36b686caf53dbfa03c5580d18d17a97f3ad30662
Chris Martin <crm28@psu.edu>
2022-07-19 17:12:41 -0400

    Drafted the worker function in post1

--- bin/post1.py
+++ bin/post1.py
@@ -3,18 +3,22 @@
 import os,sys
 import lconfig as lc
 import numpy as np
+import pickle
 
 datadir = '../data'
 
 theta_min = -0.3
 theta_max = +0.3
+Nwire = 4
 
-def worker(target):
+def worker(source, target):
     """Accepts a path to the .dat file to load and post-process
 """
 
-    conf,data = lc.load(target)
+    print('Loading...')
+    conf,data = lc.load(source)
 
+    print('Edge detection...')
     # Detect the digital input channel
     dich = int(np.log2(conf.distream))
 
@@ -23,6 +27,7 @@ def worker(target):
     # Then, get the time and current arrays
     time = data.time()
     current = data.get_channel(0)
+    Ndata = data.ndata()
     
     # The digital signal is nominally high over most of the rotation.
     # It drops when a stripe of dark tape passes under the 
@@ -47,34 +52,52 @@ def worker(target):
     # Now, starting at ii, downselect all the edges
     edges_I = edges_I[ii::4]
     # Calculate the samples between complete rotations.
-    edges_dI = np.empty_like(edges_I,dtype=float)
+    edges_dI = np.empty_like(edges_I,dtype=int)
     edges_dI[:-1] = np.diff(edges_I)
     edges_dI[-1] = edges_dI[-2]
     # If there is greater than 1% variation, there is a problem.
     if np.max(edges_dI)/np.min(edges_dI) > 1.01:
-        raise Exception('The disc speed varied by more than 1% in file: ' + target)
+        raise Exception('The disc speed varied by more than 1% in file: ' + source)
+
+    # edges_I is now an array of every index corresponding to 0rad of 
+    # disc rotation.
+    # edges_dI is an array of indices with the number of samples between
+    # edges (for a single rotation).  The last element has been 
+    # duplicated so it is the same length as edges_I.
     
-    # Finally, calculate the angular displacement per sample for each
-    # of the edges.  This value should be nearly identical for every
-    # edge, but if the disc speed shows drift, this approach will 
-    # compensate for it.
-    dtheta = 2*np.pi / edges_dI
+    print('Output...')
+    # Initialize a result - we'll write it with json
+    out = [{'theta':[], 'current':[]} for ii in range(Nwire)]
     
-    # Look for any wire events before the first edge
+    # First, look for pedestals that happened before the first wire 1 
+    # trigger event.  We'll be using the first edge pair to extrapolate.
+    I = edges_I[0]
     dI = edges_dI[0]
+    # Calculate the angle rotated between each sample
     dtheta = 2*np.pi / dI
-    # Wire 1
-    Ix = edges_I[0] - dI
-    if Ix > 0:
-        Istart = int(theta_min / dtheta)
-        Istop = int(theta_max / dtheta)
-    # Wire 2
+    # Loop through wire indices
+    for iwire in range(Nwire):
+        Izero = I - ((Nwire-iwire)*dI) // Nwire
+        Imin = max(0, Izero + int(theta_min / dtheta))
+        Imax = max(0, Izero + int(theta_max / dtheta))
+        # use ii for an iterator over individual indices
+        for ii in range( Imin, Imax ):
+            out[iwire]['theta'].append((ii - Izero) * dtheta)
+            out[iwire]['current'].append(current[ii])
     
-    # Loop over the edges
-    for I,dI in zip(edges_I[:-1], edges_dI[:-1]):
-        I1 = I
-        I2 = I1 + dI // 4
-        I3 = I1 + dI // 2
-        I4 = I1 + (dI*3) // 4
-    
-    return dt
+    # Next, loop through the other rotations
+    for I, dI, in zip(edges_I, edges_dI):
+        # Calculate the angle rotated between each sample
+        dtheta = 2*np.pi / dI
+        # Loop through wire indices
+        for iwire in range(Nwire):
+            Izero = I + (iwire*dI) // Nwire
+            Imin = min(Ndata-1, Izero + int(theta_min / dtheta))
+            Imax = min(Ndata-1, Izero + int(theta_max / dtheta))
+            # use ii for an iterator over individual indices
+            for ii in range( Imin, Imax ):
+                out[iwire]['theta'].append((ii - Izero) * dtheta)
+                out[iwire]['current'].append(current[ii])
+
+    with open(target, 'w') as ff:
+        pickle.dump(out, ff)
